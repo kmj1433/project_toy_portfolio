e92f71491c9126a3441d7ffe032ded8db6884898
김민지 <kmj1433@email.com>
2024-01-09 00:04:59 +0900

    ver1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react'
+
 import './App.css';
+import './assets/css/style.module.css'
+
+import Header from './components/Layout/Header/Header'
+import Footer from './components/Layout/Footer/Footer'
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <app>
+      <Header />
+      <Home />
+      <About />
+      <Footer />
+    </app>
   );
 }
 
